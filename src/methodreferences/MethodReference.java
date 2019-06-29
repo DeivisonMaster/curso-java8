9875f212430fd15ff9c14d3764d781370d188e33
DeivisonMaster <deivisonoliveira.info@gmail.com>
2019-06-29 15:44:36 -0400

    refactoring method reference

--- src/methodreferences/MethodReference.java
+++ src/methodreferences/MethodReference.java
@@ -3,37 +3,53 @@ package methodreferences;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class MethodReference {
 	public static void main(String[] args) {
 		
 		List<String> palavras = new ArrayList<>();
-		palavras.add("alura online");
-		palavras.add("editora casa do codigo");
+
+		palavras.add("Alura Online");
+		palavras.add("editora casa do código");
 		palavras.add("caelum");
-		
 		
 		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 		
-		// Method reference 1
-		palavras.sort(Comparator.comparing(s -> s.length()));
+		Consumer<String> consumidor = new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				System.out.println(s);
+			}
+		};
+		
+		// lambda
+		Consumer<String> consumer = s -> System.out.println(s);
 		
 		
-		// Method reference 2
-		palavras.sort(Comparator.comparing(String::length));
-		
-		
-		// Comparador destrinchado
-		Function<String, Integer> funcao = new Function<String, Integer>(){
-
+		// sem lambda
+		Function<String, Integer> function = new Function<String, Integer>(){
 			@Override
 			public Integer apply(String s) {
 				return s.length();
 			}
 		};
-		Comparator<String> comparador = Comparator.comparing(funcao);
+		Comparator<String> comparador = Comparator.comparing(function); 
 		palavras.sort(comparador);
+		
+		
+		
+		// com lambda
+		palavras.sort(Comparator.comparing(s -> s.length()));
+
+		
+		// Method Reference
+		palavras.sort(Comparator.comparing(String::length));
+		
+		//palavras.forEach(consumer);
+		
+		palavras.forEach(System.out::println);
 	}
 }
 
